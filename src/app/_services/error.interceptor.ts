45c0c5035409f1ea528edfe2b802f27583cdb90d
Mono Noor Fadhilah <halilintar@gmail.com>
2018-07-31 09:02:35 +0800

    End of Section 5

---
+++ src/app/_services/error.interceptor.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HTTP_INTERCEPTORS, HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorInterceptor implements  HttpInterceptor{
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError(err => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 401) {
+            return throwError(err.statusText);
+          }
+          const applicationError = err.headers.get('Application-Error');
+          if (applicationError) {
+            console.log(applicationError);
+            return throwError(applicationError);
+          }
+          const serverError = err.error;
+          let modalStateError = '';
+          if (serverError && typeof  serverError === 'object') {
+            for (const key in serverError) {
+              if (serverError[key]) {
+                modalStateError += serverError[key] + '\n';
+              }
+            }
+          }
+          return throwError(modalStateError || serverError || 'Server Error');
+        }
+      })
+    );
+  }
+}
+
+export const ErrorInterceptorProvider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: ErrorInterceptor,
+  multi: true
+}
