e0bc6989eddbfb632f566128b54e4e9b73329c99
Mono Noor Fadhilah <halilintar@gmail.com>
2018-07-31 18:47:26 +0800

    End of Section 9

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private baseUrl = 'http://localhost:5000/api/auth/';
+  private baseUrl = `${environment.apiUrl}auth/`;
   jwtHelper = new JwtHelperService();
   decodedToken: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model)
@@ -21,7 +23,6 @@ export class AuthService {
           if (user) {
             localStorage.setItem('token', user.token);
             this.decodedToken = this.jwtHelper.decodeToken(user.token);
-            console.log(this.decodedToken);
           }
         })
       );
